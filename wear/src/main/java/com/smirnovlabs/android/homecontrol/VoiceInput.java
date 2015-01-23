69030e3b3f9e83df63b978949ab64fc79a90524a
Ivan Smirnov <isgsmirnov@gmail.com>
2015-01-23 14:55:23 -0800

    Initial Commit. Hello World + speech to text capability.

---
+++ wear/src/main/java/com/smirnovlabs/android/homecontrol/VoiceInput.java
@@ -0,0 +1,62 @@
+package com.smirnovlabs.android.homecontrol;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class VoiceInput extends Activity {
+
+    private TextView mTextView;
+    private static final int SPEECH_REQUEST_CODE = 539890;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_input);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        // debug. later, call this from button click. or maybe just show right away?
+        displaySpeechRecognizer();
+    }
+
+
+
+    /**
+     * Create an intent that can start the Speech Recognizer activity
+     * */
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    /**
+     * This callback is invoked when the Speech Recognizer returns.
+     * This is where you process the intent and extract the speech text from the intent.
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText
+            System.out.printf("Spoken Text: %s \n", spokenText);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
