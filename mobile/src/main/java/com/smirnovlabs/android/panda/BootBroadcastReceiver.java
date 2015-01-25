10d8a414f787cccd34edf174969e5658d8149234
Ivan Smirnov <isgsmirnov@gmail.com>
2015-01-24 19:33:49 -0800

    enabled launch on boot. UNTESTED

--- mobile/src/main/java/com/smirnovlabs/android/panda/BootBroadcastReceiver.java
+++ mobile/src/main/java/com/smirnovlabs/android/panda/BootBroadcastReceiver.java
@@ -3,17 +3,21 @@ package com.smirnovlabs.android.panda;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Starts WearMessageListenerService via an intent.
  */
 public class BootBroadcastReceiver extends BroadcastReceiver {
+    private String TAG = "PANDA RECEIVER";
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "boot complete");
         // Filter for boot completed action.
         if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             Intent startServiceIntent = new Intent(context, WearMessageListenerService.class);
             context.startService(startServiceIntent);
+            Log.d(TAG, "started service");
         }
     }
 }
